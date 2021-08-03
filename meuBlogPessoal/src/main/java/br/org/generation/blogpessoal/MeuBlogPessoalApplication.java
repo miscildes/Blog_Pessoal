d9c5a90370d620211a3dcd00e21302c2c4269963
Guilherme (Gillie) <miscildes@gmail.com>
2021-08-03 07:36:54 -0300

    commit blog

---
+++ meuBlogPessoal/src/main/java/br/org/generation/blogpessoal/MeuBlogPessoalApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuBlogPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuBlogPessoalApplication.class, args);
+	}
+
+}
